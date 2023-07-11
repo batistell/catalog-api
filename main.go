c14130ec46857dc5acca5dc40e13d53beaaefdb9
gabtt <gabriel.batistell@engdb.com.br>
2023-07-11 04:48:24 -0300

    Add Swagger definations and MongoDB connection setup
    Created Swagger definations for managing product catalog and added new connection setup for MongoDB. Newly added file 'swagger.yaml' contains api paths, definitions, and responses specifications for managing product catalog. Modified main.go file to include MongoDB connection setup using 'mongo-driver'. Configuration of MongoDB connection has been defined in new file 'config.go'. Added missing depedencies in go.mod necessary for swagger and MongoDB.

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
-package catalog_api
+package main
+
+import (
+	"context"
+	"github.com/batistell/catalog-api/config"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+	"time"
+)
+
+func main() {
+	logger := logrus.New()
+	logger.Info("Initializing application...")
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logger.WithError(err).Error("Error LoadConfig")
+	}
+
+	logger.SetLevel(cfg.Logger.Level)
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetReportCaller(true)
+	logger.SetOutput(os.Stdout)
+
+	// MongoDB connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MongoDB.URI))
+	if err != nil {
+		logger.WithError(err).Error("Failed to connect to MongoDB")
+		return
+	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			logger.WithError(err).Error("Failed to disconnect from MongoDB")
+		}
+	}()
+
+	logger.Info("Connected to MongoDB")
+
+	logger.Info("Application initialized")
+}
